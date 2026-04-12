ab4e27b573713d766a65cd15b2375fe25bd26c18
Javiera Colombo <javiera.colombo@correoaiep.cl>
2026-04-12 17:59:12 -0400

    Agrego archivos Semana 4 Banco Patagonia

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password})
+    });
+
+    const data = await response.json();
+
+    document.getElementById("mensaje").innerText = data.message;
+});
